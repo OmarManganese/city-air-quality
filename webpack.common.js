8e774f724e808da53b77de5a2e5d1b1ae7190dd6
OmarManganese <78893144+OmarManganese@users.noreply.github.com>
2021-05-30 09:42:26 +0200

    Initial  Setup

---
+++ webpack.common.js
@@ -0,0 +1,26 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const Dotenv = require("dotenv-webpack");
+
+module.exports = {
+    entry: "./src/index.js",
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "City Air Quality",
+        }),
+        new Dotenv(),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+        ],
+    },
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "dist"),
+        clean: true,
+    },
+};
