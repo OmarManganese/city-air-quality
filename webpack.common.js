f7bfe90f20212717eff06b9db71ed36a9b1cf845
OmarManganese <78893144+OmarManganese@users.noreply.github.com>
2021-12-29 19:06:48 +0100

    Revert "Merge pull request #8 from OmarManganese/dev"
    This reverts commit d1ba268281a82e64e7a1829e3bf6c4737826413c, reversing
    changes made to 5d291489d3f5ba58ba030e9d1f6198f50637e4a3.

--- webpack.common.js
+++ webpack.common.js
@@ -1,14 +1,10 @@
 const path = require("path");
 const Dotenv = require("dotenv-webpack");
-const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
     entry: "./src/index.js",
     plugins: [
         new Dotenv(),
-        new HtmlWebpackPlugin({
-            template: "src/index.html"
-        })
     ],
     module: {
         rules: [
